169a936453fc6a2c9b2bf68a1f562bb3a022eec8
Carlos Hernandez <carlos.hernandez.agudelo@correounivalle.edu.co>
2023-11-02 19:18:15 -0500

    The cursor in the web page was modified

--- src/components/PixelButton.jsx
+++ src/components/PixelButton.jsx
@@ -2,10 +2,10 @@ import '../css/pixel-button.css'
 
 const PixelButton = ({ text, font, handleFunction }) => {
   return (
-    <div className={`pixel-button ${font} text-white`}>
-      <button onClick={(e) => handleFunction(e)}>
+    <div className={`nes-pointer pixel-button ${font} text-white`}>
+      <span onClick={(e) => handleFunction(e)}>
         {text}
-      </button>
+      </span>
     </div>
   )
 }
