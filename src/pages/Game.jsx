0814b86c9548ee6e34792f673c1a1b2aacb94621
Nicol Ortiz <nicolvaleria0919@gmail.com>
2023-11-04 16:20:06 -0500

    Adding component Dialogue

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -4,11 +4,13 @@ import { Canvas } from '@react-three/fiber'
 import { FPV as Fpv } from '../World/camera/FPV'
 import { Ground } from '../World/test/Ground'
 import BarMenu from '../components/BarMenu'
+import Dialogue from '../World/Dialogue'
 
 const Game = () => {
   return (
     <>
       <BarMenu />
+      <Dialogue />
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
@@ -17,6 +19,7 @@ const Game = () => {
           <Ground />
         </Physics>
       </Canvas>
+      <Dialogue titulo='Alex' text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis architecto aliquam, tenetur perspiciatis ipsum eligendi? Placeat dicta mollitia aliquid alias, officia, vel unde sequi accusamus laborum dolorum natus blanditiis nesciunt!' />
       <div className='pointer'>+</div>
     </>
   )
